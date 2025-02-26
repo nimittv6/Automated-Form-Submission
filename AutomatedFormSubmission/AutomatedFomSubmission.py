0430c1614603f36ae2b138991abcd0a37024a5be
nimitt <nimitt.verma@toorey.com>
2025-02-26 22:28:42 +0530

    Automated a form

--- AutomatedFormSubmission/AutomatedFomSubmission.py
+++ AutomatedFormSubmission/AutomatedFomSubmission.py
@@ -3,11 +3,13 @@ from xmlrpc.client import DateTime
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 web = webdriver.Chrome()
-web.get('http://localhost:63342/Python_Practice_Projects/index.html?_ijt=1icjdm98nke3m0ut7m0lttm1h9')
+web.get('http://localhost:4200/')
 time.sleep(1)
 
 sname = "Akhaury Nimitt Verma"
@@ -18,31 +20,16 @@ date_of_birth= "11/05/1999"
 dob =web.find_element('xpath', '//*[@id="dob"]')
 dob.send_keys(date_of_birth)
 
-''''# Wait for the dropdown to be clickable
+# Wait for the dropdown to be clickable
 sel_country= web.find_element('xpath', '//*[@id="country"]')
-dropdown_element = WebDriverWait(web, 10).until(EC.element_to_be_clickable(sel_country))
-
+dropdown_element = Select(WebDriverWait(web, 10).until(EC.element_to_be_clickable(sel_country)))
+dropdown_element.select_by_value("India")
 # Click the dropdown
-dropdown_element.click()
 
-time.sleep(10000)
+gender = web.find_element('xpath', '//*[@id="myForm"]/div[4]/div[1]/label')
+gender.click()
+time.sleep(5)
 
-# Wait for the options to be visible
-# option_xpath = web.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[6]/span')
-# options = WebDriverWait(web, 5).until(
-#     EC.presence_of_all_elements_located((By.XPATH, option_xpath))
-# )
-#
-# # Select an option
-# for option in options:
-#     if option.text =="Portugal":
-#         option.click()
-#         break
-# while True:
-#     pass
-#
-# sel_box = web.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div[1]/div/label/div/div[2]/div/span')
-# sel_box.click()
-#
-# submit= web.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
-# submit.click()'''
+submit = web.find_element('xpath', '//*[@id="myForm"]/button')
+time.sleep(5)
+submit.click()
